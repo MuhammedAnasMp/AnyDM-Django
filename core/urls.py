fd4f5efef31600ff02e049995bfd9b161ed015e7
Muhammed Anas <muhammedanasmp2001@gmail.com>
2026-07-05 20:57:55 +0300

    Health added

--- core/urls.py
+++ core/urls.py
@@ -2,13 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 
+def health(request):
+    return JsonResponse({"status": "ok"})
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('apps.accounts.urls')),
     path('api/products/', include('apps.products.urls')),
     path('api/automations/', include('apps.automations.urls')),
     path('api/crm/', include('apps.crm.urls')),
+    path('', health),  # 👈 add this
 ]
 
 if settings.DEBUG:
